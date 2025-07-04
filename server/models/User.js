58dcee13859fe9f23934bbed0c925bc03dd7d014
erindhoxha <hello@erindhoxha.dev>
2025-07-04 18:38:07 +0200

    Hash the passwords + salt

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -13,6 +14,41 @@ const userSchema = new mongoose.Schema({
     minlength: 6,
   },
 });
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  const { password } = user;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+  const { password } = user;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(isMatch);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
