6085d5c1091e0d4c363432644ef022a45ba25d20
erindhoxha <hello@erindhoxha.dev>
2025-07-31 03:43:11 +0200

    Use .env

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 const requireAuth = require("./middlewares/requireAuthentication");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const authRoutes = require("./routes/authRoutes");
 
 app.use(bodyParser.json());
 app.use(authRoutes);
 
-const mongoUri =
-  "mongodb+srv://admin:Helloworld1244!@cimeratcluster.qu0aygm.mongodb.net/?retryWrites=true&w=majority&appName=CimeratCluster";
+const mongoUri = process.env.MONGO_URI;
 
 mongoose.connect(mongoUri, {});
 
