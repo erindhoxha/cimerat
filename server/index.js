7aadcbb6c418c781c0e1cb745320de34725d9779
erindhoxha <hello@erindhoxha.dev>
2025-08-09 16:26:08 +0300

    Added mongo

--- server/index.js
+++ server/index.js
@@ -1,39 +1,39 @@
-require("dotenv").config();
-const express = require("express");
-const { default: mongoose } = require("mongoose");
-const bodyParser = require("body-parser");
+require('dotenv').config();
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-const requireAuth = require("./middlewares/requireAuthentication");
+const requireAuth = require('./middlewares/requireAuthentication');
 
 const port = process.env.PORT || 3000;
 
-const authRoutes = require("./routes/authRoutes");
-const listingRoutes = require("./routes/listingRoutes");
+const authRoutes = require('./routes/authRoutes');
+const listingRoutes = require('./routes/listingRoutes');
 
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(authRoutes);
 app.use(listingRoutes);
 
 const mongoUri = process.env.MONGO_URI;
 
-// mongoose.connect(mongoUri, {});
+mongoose.connect(mongoUri, {});
 
-// mongoose.connection.on("connected", () => {
-//   console.log("Connected to MongoDB successfully");
-// });
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB successfully');
+});
 
-// mongoose.connection.on("error", (err) => {
-//   console.error("Error connecting to MongoDB:", err);
-// });
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
-app.get("/", requireAuth, (_, res) => {
-  res.send("Hello from Express!");
+app.get('/', requireAuth, (_, res) => {
+  res.send('Hello from Express!');
   res.status(200);
 });
 
-app.get("/user", requireAuth, (req, res) => {
-  console.log("Getting user!");
+app.get('/user', requireAuth, (req, res) => {
+  console.log('Getting user!');
   res.status(200).json({ user: req.user });
 });
 
@@ -41,4 +41,4 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'));
